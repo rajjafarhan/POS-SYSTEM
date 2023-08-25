2bb56e6b8d60c049bbfdd4d5ac68df0611f1be0c
Rajja Farhan <rajjafarhan2@gmail.com>
2023-08-26 01:57:50 +0500

    rj1

---
+++ Frontend/src/app.jsx
@@ -0,0 +1,20 @@
+import {createRoot} from "react-dom/client";
+const App = () => {
+    return(
+        <div>
+            <h1>React </h1>
+        
+        </div>
+
+    )
+};
+
+
+
+
+
+
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />)
