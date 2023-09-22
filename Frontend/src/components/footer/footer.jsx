4527144397bac6a0e125fae33060166be2cacb83
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-09-22 18:12:30 +0500

    '..'

--- Frontend/src/components/footer/footer.jsx
+++ Frontend/src/components/footer/footer.jsx
@@ -1,9 +1,9 @@
-import './footer.css'
+import "./footer.css";
 
 const Footer = () => {
   return (
     <footer className="bg-dgreen py-3 text-yellow footer">
-      <div className="text-center fs-5">POS System 2023  ©®</div>
+      <div className="text-center fs-5">POS System 2023 ©®</div>
     </footer>
   );
 };
