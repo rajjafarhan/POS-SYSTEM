b2418dbd786c55d36dd926b20a79642b0ea87e68
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-10-21 17:10:20 +0500

    ..

--- Frontend/src/components/addButton/addButton.jsx
+++ Frontend/src/components/addButton/addButton.jsx
@@ -1,4 +1,4 @@
-import "./AddButton.css";
+import "./addButton.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
